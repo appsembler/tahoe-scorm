7232778b62f1c19ec6ed433be0d4c2f9f17c6dca
Omar Al-Ithawi <i@omardo.com>
2021-06-30 11:04:09 +0300

    bump to v0.1.1
    testing the pypi workflow

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='tahoe-scorm',
-    version='0.1.0',
+    version='0.1.1',
     description='Tahoe SCORM Customizations package.',
     packages=[
         'tahoe_scorm',
