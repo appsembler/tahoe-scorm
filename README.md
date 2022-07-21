a30c837b89a57428f9d5797cbca00487be02b7cf
Shadi Naif <shadinaif@gmail.com>
2022-07-21 11:47:19 +0300

    Fix long-description causing publish to fail

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ Tahoe SCORM XBlock Customizations: Storage Backends.
 
 
 # Change Log
- - `v0.1.3` Fix compatiblity with Tahoe 2.0 configurations
+ - `v0.1.3` Fix compatibility with Tahoe 2.0 configurations
  - `v0.1.2` Fix ImportError on Python 3.5
  - `v0.1.1` First release
--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 """Set up for the Tahoe SCORM Customizations package."""
 
+from pathlib import Path
 from setuptools import setup
+
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='tahoe-scorm',
@@ -9,4 +14,6 @@ setup(
     packages=[
         'tahoe_scorm',
     ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
