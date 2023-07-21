8b16adfbf0ec7185d6f53426ac991354a9884045
Bryan Wilson <bryanlandia@gmail.com>
2023-07-21 09:00:21 -0700

    Make course=-org based storage_loc and domain optional, defaulting to On
    changelog v0.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='tahoe-scorm',
-    version='0.1.4',
+    version='0.2.0',
     description='Tahoe SCORM Customizations package.',
     packages=[
         'tahoe_scorm',
